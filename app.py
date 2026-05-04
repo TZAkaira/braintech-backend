99aab2ed4b012f10a4e600a841af783e0e0e8109
Tayyaba Zia <tayyabazia001@gmail.com>
2026-05-04 22:55:36 +0500

    Fix port for Railway

--- app.py
+++ app.py
@@ -289,6 +289,11 @@ def home():
 if __name__ == "__main__":
     app.run(debug=True)
 
+# Naya:
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
 
 '''cd backend
 venv311\Scripts\activate
